d4f3d5f0f9c3312e95160cfcb8af3af28fc5b715
Deivis Alvarez <defaaldi@gmail.com>
2024-09-24 16:00:30 -0500

    Update MyfourthComponentOnFocusOnBlur

--- src/components/MyFourthComponent.jsx
+++ src/components/MyFourthComponent.jsx
@@ -17,6 +17,18 @@ export const MyFourthComponent = () => {
     alert("Haz SALIDO de la Caja");
   };
 
+  const handleMouse = (e, action) => {
+    alert(`Has ${action} la caja`);
+  };
+
+  const handleInsideInput = (e) => {
+    console.log("Estas dentro del input, escribe tu nombre!!");
+  };
+
+  const handleOutsideInput = (e) => {
+    console.log("Estás fuera del input, Adiós!!");
+  };
+
   return (
     <div>
       <h1>MyFourthComponent</h1>
@@ -47,10 +59,24 @@ export const MyFourthComponent = () => {
       </div>
       <div
         id="box1"
+        className="mt-4"
         onMouseEnter={(e) => handleMouseEnter(e)}
         onMouseLeave={(e) => handleMouseleave(e)}
       >
         <p>Pasa el mause por la caja azul</p>
+      </div>
+      <div
+        id="box2"
+        className="mt-4"
+        onMouseEnter={(e) => handleMouse(e, "Entrado a")}
+        onMouseLeave={(e) => handleMouse(e, "salido de")}
+      ></div>
+      <div className="mt-4">
+        <input
+          type="text"
+          onFocus={(e) => handleInsideInput(e)}
+          onBlur={(e) => handleOutsideInput(e)}
+        />
       </div>
     </div>
   );
