da883443ab8b8fbe94de077e6a85b69c9ce0639d
Deivis Alvarez <defaaldi@gmail.com>
2024-09-24 15:07:37 -0500

    UpdateMyfouthComponentDoubleClick

--- src/components/MyFourthComponent.jsx
+++ src/components/MyFourthComponent.jsx
@@ -1,18 +1,42 @@
 import React from "react";
 
 export const MyFourthComponent = () => {
+  const handleClicked = (e, name) => {
+    alert("Haz hecho click en el botón: " + name);
+  };
+
+  const handleDoubleClicked = () => {
+    alert("Haz hecho doble click");
+  };
+
   return (
     <div>
-      <div>MyFourthComponent</div>
-      <button
-        id="UNO"
-        className="btn btn-danger"
-        onClick={() => {
-          alert("Soy un evento click");
-        }}
-      >
-        Haz Clic
-      </button>
+      <h1>MyFourthComponent</h1>
+      <div>
+        <button
+          className="btn btn-danger"
+          onClick={() => {
+            alert("Soy un evento click");
+          }}
+        >
+          Haz Clic
+        </button>
+      </div>
+
+      <div>
+        <button
+          className="btn btn-success"
+          onClick={(e) => handleClicked(e, "DEIVIS")}
+        >
+          ¡Aqui También haz Clic!
+        </button>
+      </div>
+
+      <div>
+        <button className="btn btn-info" onDoubleClick={handleDoubleClicked}>
+          ¡Haz doble Clic!
+        </button>
+      </div>
     </div>
   );
 };
